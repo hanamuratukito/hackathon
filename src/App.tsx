a79efe45fb12bded021b117b7bc7117f7fb7b047
hanamura <keikennouta11@gmail.com>
2024-09-19 18:44:58 +0900

    update：h1要素変更

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>ハッカソン用画面</h1>
       <div className="card">
         <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
